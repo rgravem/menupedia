4fdbb6e556834f029b34ba25e68b2402cb8ba0d9
Ross Gravem <rgravem@yahoo.com>
2016-10-03 16:29:22 -0500

    sourced in bootstrap

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -34,3 +34,60 @@ myApp.config(['$routeProvider', function($routeProvider){
         redirectTo: "/home"
       });
 }]);
+
+myApp.controller('authController', ['$scope', '$http', function($scope, $http){
+  console.log('NG');
+// run controller on load
+  $scope.init = function(){
+    console.log('in init');
+    //check for user info in localStorage
+    if( JSON.parse(localStorage.getItem( 'userProfile' ))){
+      // if found, save userProfile as $scope.userProfile
+      $scope.userProfile = JSON.parse( localStorage.getItem( 'userProfile'));
+      console.log( 'logged in as:', $scope.userProfile );
+      $scope.showUser = true;
+    }else{
+      // if no info saved, make sure empty
+      emptyLocalStorage();
+      $scope.showUser = false;
+    }
+  }; // end init
+
+  $scope.logIn = function(){
+  console.log('in logIn');
+  lock.show( function( err, profile, token ){
+    if(err){
+      console.log('err:', err);
+    }else{
+      localStorage.setItem( 'userToken', token );
+      console.log( 'token:', token );
+      localStorage.setItem( 'userProfile', JSON.stringify( profile ) );
+      console.log('Auth0 success, Profile:', profile);
+      location.reload();
+    }
+  });
+}; // end logIn
+$scope.logOut = function(){
+    // call out logOutUrl
+    emptyLocalStorage();
+    location.reload();
+    // $http({
+    //   method: 'GET',
+    //   url: logOutUrl,
+    // }).then( function(data){
+    //   // if logged out OK
+    //   if(data.data == 'OK'){
+    //     // empty localStorage
+    //     emptyLocalStorage();
+    //     $scope.showUser = false;
+    //   }
+    // });
+  }; // end logout
+
+$scope.init();
+}]);
+
+var emptyLocalStorage = function(){
+  localStorage.removeItem('userProfile');
+  localStorage.removeItem( 'userToken' );
+};
