d43babfe874272c1201bece11a68ff150eabd9aa
Ross Gravem <rgravem@yahoo.com>
2016-10-12 16:08:18 -0500

    logging out quiz fields successufully

--- public/scripts/controllers/quizController.js
+++ public/scripts/controllers/quizController.js
@@ -8,6 +8,7 @@ myApp.controller('quizController', ['$scope', '$http', function($scope, $http) {
       $scope.admin = false;
     }
   }; // end checkAdmin
+  var questionM = [];
   $scope.questionM = [];
   $scope.questionA = [];
   $scope.addMultiQ = function(){
@@ -15,11 +16,31 @@ myApp.controller('quizController', ['$scope', '$http', function($scope, $http) {
       question: "",
       text: ""
     });
-  };
+  };// end addMultiQ
   $scope.addShortA = function(){
     $scope.questionA.push({
-      // question: "",
-      // text: ""
+      question: "",
+      text: ""
     });
+  }; // end addShortA
+
+  $scope.addQuestion = function(shortA){
+    console.log('addQuiz button hit');
+    console.log('answer:',shortA.value);
+    console.log('question:', shortA.question);
+    var question = {
+      question: shortA.question,
+      answer: shortA.value
+    };
+  questionM.push(question);
+  }; // end addQuestion
+
+  $scope.addQuestionM = function(multi){
+    console.log('multi add hit');
+    console.log('question:', multi.question);
+    console.log('answers:', multi.optA);
+    if(multi.correctAnswerA){
+      console.log('a is right');
+    }else{}
   };
-}]);
+}]); // end quizController
